541b6e8334644365394d67b42a31758a14803e1a
peihao <pei.h@northeastern.edu>
2024-10-12 15:41:54 -0700

    imported DropDownPicker, and added the picker component in AddActivity.js

--- Screens/AddActivity.js
+++ Screens/AddActivity.js
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState } from 'react';
+
 
 export default function AddActivity() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [activities, setActivities] = useState([
+    {label: 'Walking', value: 'Walking'},
+    {label: 'Running', value: 'Running'},
+    {label: 'Swimming', value: 'Swimming'},
+    {label: 'Weights', value: 'Weights'},
+    {label: 'Yoga', value: 'Yoga'},]);
   return (
     <View>
-      <Text>AddActivity</Text>
+      <DropDownPicker
+        placeholder="Select An Activity"
+        open={open}
+        value={value}
+        items={activities}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setActivities}
+      />
     </View>
   )
 }
