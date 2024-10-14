6c1e33a9cca3e069f1e765aaca763dd848bf1d71
peihao <pei.h@northeastern.edu>
2024-10-13 17:26:36 -0700

    use a boolean variable to decide if the reminder icon is rendered, the boolean is decided by the activity or diet requirements.

--- Components/ListEntry.js
+++ Components/ListEntry.js
@@ -1,61 +1,79 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { StyleHelper } from './StyleHelper';
+import { StyleHelper, ColorHelper } from './StyleHelper';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function ListEntry({ itemObj }) {
-  return (
-    <View style={styles.boxStyle}>
+    const activityData = itemObj?.activity ?? null;
+    const durationData = itemObj?.duration ? parseInt(itemObj.duration) : null;
+    const caloriesData = itemObj?.calories ? parseInt(itemObj.calories) : null;
 
-      <View style={styles.activityBox}>
-        {itemObj.activity && <Text style={styles.textEntry}>{itemObj.activity}</Text>}
+    let showActivityIcon = (activityData === 'Running'
+                    || activityData === 'Weights') && (durationData > 60);
 
-        {itemObj.description && <Text style={styles.textEntry}>{itemObj.description}</Text>}
+    let showDietIcon = caloriesData > 800;
 
-      </View>
+    return (
+        <View style={styles.boxStyle}>
+
+            <View style={styles.activityBox}>
+                {/* activity data and reminder icon */}
+                {itemObj.activity && <Text style={styles.textEntry}>{itemObj.activity}</Text>}
+                {showActivityIcon && <MaterialCommunityIcons name='alert' size={24} color={ColorHelper.activeTabColor} />}
+
+                {/* diet data and reminder icon */}
+                {itemObj.description && <Text style={styles.textEntry}>{itemObj.description}</Text>}
+                {showDietIcon && <MaterialCommunityIcons name='alert' size={24} color={ColorHelper.activeTabColor} />}
+
+            </View>
 
 
-      <View style={styles.calorieBox}>
-        {itemObj.date && <Text>{itemObj.date}</Text>}
-      </View>
+            <View style={styles.calorieBox}>
+                {itemObj.date && <Text>{itemObj.date}</Text>}
+            </View>
 
 
-      <View style={[styles.calorieBox, { width: '15%' }]}>
-        {itemObj.duration && <Text>{itemObj.duration}</Text>}
-        {itemObj.calories && <Text>{itemObj.calories}</Text>}
-      </View>
-    </View>
-  );
+            <View style={[styles.calorieBox, { width: '15%' }]}>
+                {itemObj.duration && <Text>{itemObj.duration}</Text>}
+                {itemObj.calories && <Text>{itemObj.calories}</Text>}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  boxStyle: {
-    width: '100%',
-    height: 50,
-    padding: 5,
-    backgroundColor: 'rgba(3, 201, 169, 1)',
-    borderRadius: 5,
-    flexDirection: 'row', // Arrange elements in a row
-    alignItems: 'center', // Align elements vertically center
-    justifyContent: 'flex-start', // Spread items evenly with space between
-  },
-  activityBox: {
-    width: '40%',
-    justifyContent: 'center',
-  },
-  calorieBox: {
-    width: '40%',
-    height: 30,
-    padding: 5,
-    marginLeft: 5,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textEntry: {
-    color: 'white',
-    fontSize: 13,
-    fontWeight: 'bold',
-    paddingLeft: 5,
-  },
+    boxStyle: {
+        width: '100%',
+        height: 50,
+        padding: 5,
+        margin: 5,
+        backgroundColor: 'rgba(3, 201, 169, 1)',
+        borderRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    activityBox: {
+        width: '40%',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
+    },
+    calorieBox: {
+        width: '40%',
+        height: 30,
+        padding: 5,
+        marginLeft: 5,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textEntry: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+        paddingLeft: 5,
+    },
 });
--- Screens/AddDiet.js
+++ Screens/AddDiet.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 import { useContext } from 'react';
