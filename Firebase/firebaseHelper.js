f28d154f4b36ad8f28665c1c51056a6f90cd7065
peihao <pei.h@northeastern.edu>
2024-10-27 02:01:35 -0700

    add a delete button at the headerRight. deleteDoc would be called if the delete button is pressed.

--- Firebase/firebaseHelper.js
+++ Firebase/firebaseHelper.js
@@ -1,21 +1,34 @@
-import { collection, addDoc, doc, updateDoc } from 'firebase/firestore';
+import {
+    collection, addDoc,
+    doc, updateDoc, deleteDoc
+} from 'firebase/firestore';
 import { database } from './firebaseSetup';
 
 
 export async function writeToDB(collectionName, data) {
-  try {
-    const docRef = await addDoc(collection(database, collectionName), data);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
+    try {
+        const docRef = await addDoc(collection(database, collectionName), data);
+    } catch (e) {
+        console.error('Error adding document: ', e);
+    }
 }
 
 export async function updateDB(collectionName, docId, data) {
-  try {
-    const docNew = doc(database, collectionName, docId);
-    await updateDoc(docNew, data);
+    try {
+        const docNew = doc(database, collectionName, docId);
+        await updateDoc(docNew, data);
 
-  } catch (e) {
-    console.error('Error updating document: ', e);
-  }
+    } catch (e) {
+        console.error('Error updating document: ', e);
+    }
+}
+
+export async function deleteDB(collectionName, docId) {
+    try {
+        const docNew = doc(database, collectionName, docId);
+        await deleteDoc(docNew);
+
+    } catch (e) {
+        console.error('Error deleting document: ', e);
+    }
 }
--- Screens/Edit.js
+++ Screens/Edit.js
@@ -1,17 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Button } from 'react-native'
+import React, { useEffect } from 'react'
 import BackgroundContainer from '../Components/BackgroundContainer'
 import AddActivity from './AddActivity'
 import AddDiet from './AddDiet'
+import { deleteDB } from '../Firebase/firebaseHelper'
 
-export default function Edit({route}) {
+
+export default function Edit({navigation, route}) {
+
+  function handleDelete() {
+    let collectionName = route.params.itemData.activity ? 'activity' : 'diet';
+    deleteDB(collectionName, route.params.itemData.id);
+    navigation.navigate(route.params.itemData.activity ? 'Activity' : 'Diet');
+  }
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight : () => (
+        <Button
+          title="delete"
+          onPress={handleDelete}>
+        </Button>
+      ),
+    });
+  }
+  );
+
   return (
     <BackgroundContainer>
       {route.params.itemData.activity ?
       <AddActivity itemData={route.params.itemData} />
       : <AddDiet itemData={route.params.itemData} />}
     </BackgroundContainer>
-
   )
 }
 
